c33a7b6d262df6e542b786588c816f1dcd15df0e
giannhs2454 <gparadisis96@gmail.com>
2024-06-22 12:17:07 +0300

    leaflet (add map)

--- smellycat/src/app/app.component.ts
+++ smellycat/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'smellycat';
+  map!: L.Map;
+
+  ngOnInit(): void {
+    this.initializeMap();
+  }
+
+  initializeMap(): void {
+    this.map = L.map('map').setView([40.638435, 22.947009], 14);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map);
+
+    // Use this.map instead of map here
+    var marker = L.marker([40.638435, 22.947009]).addTo(this.map);
+  }
 }
